bc0e6c5cfbc9f99717adcc3c8888d67ab5dfba24
Amir Rafati <rafatesmino@gmail.com>
2024-12-11 13:06:38 +0330

    Update `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      name: 'Vue3PulToRefresh',
-      fileName: 'vue3-pull-to-refresh'
+    plugins: [vue()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "lib/main.ts"),
+            name: "Vue3PullToRefresh",
+            fileName: "vue3-pull-to-refresh",
+        },
+        rollupOptions: {
+            external: ["vue"],
+            output: {
+                globals: {
+                    vue: "Vue",
+                },
+                // Ensure that the styles are bundled if needed
+                assetFileNames: "assets/[name]-[hash][extname]",
+            },
+        },
     },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
-})
+});
